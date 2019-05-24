9604d8e004f7313a71b02b29692cfa6365df1ded
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-05-25 01:10:21 +0800

    Refactored torrent object
    The properties are variable for further extension

--- autoremovetorrents/client/qbittorrent.py
+++ autoremovetorrents/client/qbittorrent.py
@@ -69,13 +69,19 @@ class qBittorrent(object):
                 # Get other information
                 properties = self._torrent_generic_properties(torrent_hash)
                 trackers = self._torrent_trackers(torrent_hash)
-                return Torrent(
-                    torrent['hash'], torrent['name'],
-                    torrent['category'] if 'category' in torrent else torrent['label'],
-                    [tracker['url'] for tracker in trackers],
-                    qBittorrent._judge_status(torrent['state']), torrent['size'], torrent['ratio'],
-                    properties['total_uploaded'], properties['addition_date'],
-                    properties['seeding_time'])
+                # Create torrent object
+                torrent_obj = Torrent()
+                torrent_obj.hash = torrent['hash']
+                torrent_obj.name = torrent['name']
+                torrent_obj.category = torrent['category'] if 'category' in torrent else torrent['label']
+                torrent_obj.tracker = [tracker['url'] for tracker in trackers]
+                torrent_obj.status = qBittorrent._judge_status(torrent['state'])
+                torrent_obj.size = torrent['size']
+                torrent_obj.ratio = torrent['ratio']
+                torrent_obj.uploaded = properties['total_uploaded']
+                torrent_obj.create_time = properties['addition_date']
+                torrent_obj.seeding_time = properties['seeding_time']
+                return torrent_obj
 
     # Judge Torrent Status (qBittorrent doesn't have stopped status)
     @staticmethod
