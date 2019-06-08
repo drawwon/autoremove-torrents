a294774e1152b1d0e61a444b63a04172006d93b2
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2019-06-08 23:59:51 +0800

    Added upload and download speed condition

--- autoremovetorrents/strategy.py
+++ autoremovetorrents/strategy.py
@@ -7,6 +7,8 @@ from .filter.status import StatusFilter
 from .condition.seedingtime import SeedingTimeCondition
 from .condition.createtime import CreateTimeCondition
 from .condition.ratio import RatioCondition
+from .condition.downloadspeed import DownloadSpeedCondition
+from .condition.uploadspeed import UploadSpeedCondition
 from .condition.torrentsize import TorrentSizeCondition
 from .condition.torrentnumber import TorrentNumberCondition
 from .condition.donothing import EmptyCondition
@@ -65,6 +67,8 @@ class Strategy(object):
             'seeding_time': SeedingTimeCondition,
             'create_time': CreateTimeCondition,
             'ratio': RatioCondition,
+            'min_downloadspeed': DownloadSpeedCondition,
+            'min_uploadspeed': UploadSpeedCondition,
             'seed_size': TorrentSizeCondition,
             'maximum_number': TorrentNumberCondition,
             'nothing': EmptyCondition
